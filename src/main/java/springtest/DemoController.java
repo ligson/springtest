6f3ffa1a0068a0653fa5e749402ad7e2c7dbf904
ligson <593949938@qq.com>
2016-07-27 18:01:11 +0800

    sse

--- src/main/java/springtest/DemoController.java
+++ src/main/java/springtest/DemoController.java
@@ -1,12 +1,7 @@
 package springtest;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -23,65 +18,83 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEvent
 @Controller
 public class DemoController {
 
-	private static List<SseEmitter> emitters = new ArrayList<>();
-	private HttpServletRequest request;
-	private HttpServletResponse response;
+    private static List<SseEmitter> emitters = Collections.synchronizedList(new ArrayList<>());
+    private HttpServletRequest request;
+    private HttpServletResponse response;
 
-	@ModelAttribute
-	public void setBase(HttpServletRequest request, HttpServletResponse response) {
-		this.request = request;
-		this.response = response;
-	}
+    @ModelAttribute
+    public void setBase(HttpServletRequest request, HttpServletResponse response) {
+        this.request = request;
+        this.response = response;
+    }
 
-	@RequestMapping("/index.do")
-	public String index() {
-		return "index";
-	}
+    @RequestMapping("/index.do")
+    public String index() {
+        return "index";
+    }
 
-	@ResponseBody
-	@RequestMapping("/add.do")
-	public Message addData(String msg) {
-		Message message = new Message();
-		message.setMessage(msg);
-		message.setFrom(request.getRemoteHost());
-		Iterator<SseEmitter> iter = emitters.iterator();
-		while(iter.hasNext()){
-			SseEmitter emitter = iter.next();
-			System.out.println(emitter.hashCode() + "===准备发送消息"+message);
-			try {
+    class SendThread implements Runnable {
+        private Message message;
 
-				emitter.send(message);
-				System.out.println(emitter.hashCode() + "===消息发送成功");
-			} catch (Exception e) {
-				System.out.println("给客户端发送消息失败"+emitter);
-				//emitter.complete();
-				iter.remove();
-				//emitters.remove(emitter);
-				//e.printStackTrace();
-				System.out.println(emitter.hashCode() + "===客户端移除");
-			}
+        public SendThread(Message message) {
+            this.message = message;
+        }
 
-		}
-		System.out.println(emitters.size()+" 客户端个数");
-		return message;
-	}
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+            }
+            List<SseEmitter> waitRemoveList = new ArrayList<>();
+            Iterator<SseEmitter> iter = emitters.iterator();
+            while (iter.hasNext()) {
+                SseEmitter emitter = iter.next();
+                System.out.println(emitter.hashCode() + "===准备发送1消息" + message);
+                try {
+                    emitter.send(message,MediaType.TEXT_PLAIN);
+                    System.out.println(emitter.hashCode() + "===消息发送成功");
+                } catch (Exception e) {
+                    System.out.println("给客户端发送消息失败" + emitter);
+                    System.out.println(emitter.hashCode() + "===客户端移除");
+                    waitRemoveList.add(emitter);
+                }
+            }
+            waitRemoveList.forEach(e -> emitters.remove(e));
+            System.out.println(emitters.size() + " 客户端个数");
+        }
+    }
 
-	@RequestMapping(path = "/pull.do", method = RequestMethod.GET)
-	public SseEmitter pull() {
-		SseEmitter emitter = new SseEmitter((long) (60 * 1000));
-		System.out.println(emitter.hashCode() + "启动sseEmitter");
-		emitter.onCompletion(() -> emitters.remove(emitter));
-		// emitter.onTimeout(() -> System.out.println("timeout........"));
-		emitters.add(emitter);
-		System.out.println("现在客户端个数："+emitters.size());
-		return emitter;
-	}
+    @ResponseBody
+    @RequestMapping("/add.do")
+    public Message addData(String msg) {
+        Message message = new Message();
+        message.setMessage(msg);
+        message.setFrom(request.getRemoteHost());
+        Thread thread = new Thread(new SendThread(message));
+        thread.start();
+        System.out.println("............return");
+        return message;
+    }
 
-	@RequestMapping("/json.do")
-	@ResponseBody
-	public Map<String, Object> json() {
-		Map<String, Object> result = new HashMap<>();
-		result.put("msg", new Date());
-		return result;
-	}
+    @RequestMapping(path = "/pull.do", method = RequestMethod.GET)
+    public SseEmitter pull() {
+        SseEmitter emitter = new SseEmitter((long) (600 * 1000));
+        System.out.println(emitter.hashCode() + "启动sseEmitter");
+        emitter.onCompletion(() -> emitters.remove(emitter));
+        emitter.onTimeout(() -> emitters.remove(emitter));
+        emitters.add(emitter);
+        System.out.println("现在客户端个数：" + emitters.size());
+        return emitter;
+    }
+
+    @RequestMapping("/json.do")
+    @ResponseBody
+    public Message json(String msg) {
+        Message message = new Message();
+        message.setFrom(request.getRemoteHost());
+        message.setMessage(msg);
+        return message;
+    }
 }
--- src/main/webapp/WEB-INF/pages/index.jsp
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -1,32 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
+         pageEncoding="UTF-8" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>111</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>111</title>
+    <script type="text/javascript">
+        var sse = new EventSource("http://localhost:8888/springtest/pull.do");
+        sse.onmessage = function (e) {
+            console.log("=-----" + e.data);
+        };
+        sse.onopen = function (e) {
+            console.log("open connection");
+        };
+        sse.onerror = function (e) {
+            console.log("error-" + e);
+        };
+    </script>
 </head>
 <body>
-	<script type="text/javascript">
-		var sse = new EventSource("http://localhost:8080/springtest/pull.do");
-		/* sse.addEventListener("message", function(e) {
-			console.log(e);
-		});
-		sse.addEventListener('open', function(e) {
-			console.log("Connection was opened.");
-		}, false);
-
-		sse.addEventListener('error', function(e) {
-			if (e.readyState == EventSource.CLOSED) {
-				console.log("Connection was closed.");
-			} else {
-				console.log(e.readyState);
-			}
-		}, false); */
-		console.log("==========iit...");
-		sse.onmessage = function(e){
-			console.log("=-----"+e);
-		};
-	</script>
 </body>
 </html>
